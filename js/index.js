686ba4ba32daaf447360e0bbb4115250c5b851db
Julien <julien.linder@gmail.com>
2017-09-08 09:18:04 +0200

    mise en place package qui détecte ce que la souris sélection, ne marche pas encore

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-document.querySelector('.cdclickable').addEventListener('click', function(evt){
-	console.log('This is 2D element was clicked!');
-});
-	
-});
+// 	var sceneEl = document.querySelector('a-scene').getElementsByTagName("#a-plane").addEventListener('click', function(evt){
+// 	console.log(sceneE1);
+	// console.log(sceneEl.querySelector('#plane'));	
+// });;
+// });
+// var planne = document.getElementById("scene").getElementsByTagName("clickable");
+// document.querySelector('a-plane').flushToDOM().addEventListener('click', function(evt){
+// 	console.log('This is 2D element was clicked!');
+// });
+// document.querySelector('a-sphere').addEventListener('click', function(evt){
+// 	console.log('This is 3D element was clicked!');
+// });
+// $(document).ready('click', function(evt){
+// 	document.getElementById("scene").getElementsByTagName("clickable");
+// 	document.querySelector('clickable').addEventListener('click');     
+// 	console.log('This is 2D element was clicked!');
+//       });
