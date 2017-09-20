e63cc12aef31fdb79ed8668f2c1d4d966460531f
Julien <julien.linder@gmail.com>
2017-09-20 10:11:17 +0200

    nouvelle orientation: réalité augmentée&site

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 
-// 	var sceneEl = document.querySelector('a-scene').getElementsByTagName("#a-plane").addEventListener('click', function(evt){
-// 	console.log(sceneE1);
-	// console.log(sceneEl.querySelector('#plane'));	
-// });;
-// });
+	var sceneEl = document.querySelector('a-scene').addEventListener('click', function(evt){
+	console.log(sceneEl);	
+	window.location.replace('test.html');
+});
+});
 // var planne = document.getElementById("scene").getElementsByTagName("clickable");
 // document.querySelector('a-plane').flushToDOM().addEventListener('click', function(evt){
 // 	console.log('This is 2D element was clicked!');
@@ -12,8 +12,10 @@
 // document.querySelector('a-sphere').addEventListener('click', function(evt){
 // 	console.log('This is 3D element was clicked!');
 // });
-// $(document).ready('click', function(evt){
+//  $(document).ready(function(){
 // 	document.getElementById("scene").getElementsByTagName("clickable");
-// 	document.querySelector('clickable').addEventListener('click');     
-// 	console.log('This is 2D element was clicked!');
+// 	document.querySelector('a-scene').addEventListener('click', function(evt){
+// 	return "index.html".innerHTML("href");
+		
+// 	});     
 //       });
