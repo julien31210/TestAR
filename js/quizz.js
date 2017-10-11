2eea3a6be809964e6ac94bc1b17352864f64f824
Julien <julien.linder@gmail.com>
2017-10-11 16:55:30 +0200

    drag&drop fonctionne

--- js/quizz.js
+++ js/quizz.js
@@ -1,9 +1,27 @@
 $(document).ready(function(e){
 	$(".draggable").draggable({
-		// refreshPositions: true;
+		stop: handleDragStop
 	});
+	$("draggable").on("dragstop", function(event, ui){
+		console.log("ok");
+	});
+
+	function handleDragStop( event, ui ) {
+		var offsetXPos = parseInt( ui.offset.left );
+		var offsetYPos = parseInt( ui.offset.top );
+		// alert( "Drag stopped!\n\nOffset: (" + offsetXPos + ", " + offsetYPos + ")\n");
+
+		var position = $(".tableau")[0].getBoundingClientRect();
+		var x = position.left + position.width;
+		var y = position.top + position.height;
+
+		if(offsetXPos < x && offsetYPos < y){
+			alert("ok");
+		}
+	}
+});
+// refreshPositions: true;
 // 	var refreshPositions = $( ".draggable" ).draggable( "option", "refreshPositions" );
 
 // // Setter
 // $( ".draggable" ).draggable( "option", "refreshPositions", true );
-});
