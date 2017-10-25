58f25a8b6d7720f75e21a9acc6f16ab739a280f3
Julien <julien.linder@gmail.com>
2017-10-25 17:21:12 +0200

    savoir dans quelles div sont déplacé les élements, en cours...

--- js/quizz.js
+++ js/quizz.js
@@ -2,9 +2,9 @@ $(document).ready(function(e){
 	$(".draggable").draggable({
 		stop: handleDragStop
 	});
-	$("draggable").on("dragstop", function(event, ui){
-		console.log("ok");
-	});
+	$( ".liondrag" ).droppable( function(){
+		console.log('coucou');
+	} );
 
 	function handleDragStop( event, ui ) {
 		var offsetXPos = parseInt( ui.offset.left );
@@ -12,16 +12,32 @@ $(document).ready(function(e){
 		// alert( "Drag stopped!\n\nOffset: (" + offsetXPos + ", " + offsetYPos + ")\n");
 
 		var position = $(".tableau")[0].getBoundingClientRect();
+		// console.log(position);
 		var x = position.left + position.width;
 		var y = position.top + position.height;
 
 		if(offsetXPos < x && offsetYPos < y){
-			alert("ok");
+			console.log(offsetXPos, offsetYPos);
+			if (offsetXPos < x && offsetYPos < y) {
+
+			};
+
+			var id= $(this).data('response');
+			console.log(id);
+			switch(id){
+				case "liondrag":
+				console.log('ça marche');
+				break;
+				case "flute":
+				console.log('lolololol')
+				break
+				default:
+				console.log('ce n\'est pas une image');
+			};
 		}
 	}
 });
 // refreshPositions: true;
 // 	var refreshPositions = $( ".draggable" ).draggable( "option", "refreshPositions" );
-
 // // Setter
 // $( ".draggable" ).draggable( "option", "refreshPositions", true );
