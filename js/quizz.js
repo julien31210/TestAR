a1c2121206b9a77c3ff12e2a0f94a405975ee28a
Julien <julien.linder@gmail.com>
2017-10-26 17:23:17 +0200

    savoir dans quelles div sont déplacé les élements, en cours...

--- js/quizz.js
+++ js/quizz.js
@@ -11,7 +11,7 @@ $(document).ready(function(e){
 		var offsetYPos = parseInt( ui.offset.top );
 		// alert( "Drag stopped!\n\nOffset: (" + offsetXPos + ", " + offsetYPos + ")\n");
 
-		var position = $(".tableau")[0].getBoundingClientRect();
+		var position = $(".lion")[0].getBoundingClientRect();
 		// console.log(position);
 		var x = position.left + position.width;
 		var y = position.top + position.height;
